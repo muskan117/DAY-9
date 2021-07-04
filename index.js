013c9876e61b647639c88525cd25e6532666c763
muskan <muskan.1923csi1077@kiet.edu>
2021-07-04 11:00:42 +0530

    first commit

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-var booked=0;
-var Remaining=57;
+var booked = 0;
+var available = 57;
 document.getElementById("booked").innerHTML = booked;
 document.getElementById("available").innerHTML = available;
-let container=document.getElementById("container");
-for(let i=0;i<57;i++)
-{
-    let box=document.createElement("div");
-    box.className="box";
+let container = document.getElementById("container");
+
+//Boxes inside the container class
+for (let i = 0; i < 57; i++) {
+    let box = document.createElement("div");
+    box.className = "box";
     container.appendChild(box);
 }
-let boxes=document.querySelectorAll(".box");
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        if(box.classList.contains("selected"))
-        {
+
+let boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+
+        //When the person click on the seat twice
+        if (box.classList.contains("selected")) {
             box.classList.remove("selected");
             booked--;
-            Remaining++;
+            available++;
             document.getElementById("booked").innerHTML = booked;
             document.getElementById("available").innerHTML = available;
         }
-        else{
+
+        //When the person click on the seat
+        else {
             box.classList.add("selected");
             booked++;
-            Remaining--;
+            available--;
             document.getElementById("booked").innerHTML = booked;
             document.getElementById("available").innerHTML = available;
         }
     })
-})
+});
